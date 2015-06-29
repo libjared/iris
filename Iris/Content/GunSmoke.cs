d5590fb61c059bcbb011c6a27ad704cb0b78e82f
Bear <jmcgraw@purdue.edu>
2015-06-29 00:50:35 -0400

    Starting Gun Smoke

---
+++ Iris/Content/GunSmoke.cs
@@ -0,0 +1,29 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Iris
+{
+    public class GunSmoke : GameObject
+    {
+        public Animation animation;
+
+        public GunSmoke() :base()
+        {
+           // animation = new Animation(Content.GetTexture("smoke"))
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+        }
+    }
+}
