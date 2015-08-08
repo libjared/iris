a836c6ae92c4578d4f44e2bf2c87325850000e56
sourcerect <hypershadsy@gmail.com>
2015-08-07 23:32:09 -0400

    menu cleanups

--- Iris/Classes/Menu.cs
+++ Iris/Classes/Menu.cs
@@ -1,4 +1,4 @@
-﻿using SFML.Graphics;
+using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
 using System;
@@ -22,10 +22,30 @@ namespace Iris
 
         public StringBuilder activeField = new StringBuilder("");
 
+        private RectangleShape rectConnect;
+        private RectangleShape rectIP;
+        private RectangleShape rectUsername;
+
         public Menu()
             : base()
         {
             shader = new RenderStates(new Shader(null, "Content/bgPrlx.frag"));
+
+            rectConnect = new RectangleShape()
+            {
+                Size = new Vector2f(150, 30),
+                Position = new Vector2f(-25, 70)
+            };
+            rectIP = new RectangleShape()
+            {
+                Size = new Vector2f(150, 20),
+                Position = new Vector2f(-25, 40)
+            };
+            rectUsername = new RectangleShape()
+            {
+                Size = new Vector2f(150, 20),
+                Position = new Vector2f(-25, 10)
+            };
 
             MainGame.window.TextEntered += TextEnteredEvent;
         }
@@ -64,12 +84,12 @@ namespace Iris
 
         public override void Update()
         {
-            //Main menu logic
-            //Probably just a username and an IP to connect to
+            //move the train
             trainPosX += .7f;
             if (trainPosX > 400)
                 trainPosX = -250f;
 
+            //handle paste
             if (Clipboard.ContainsText())
             {
                 if (Input.isKeyDown(Keyboard.Key.LControl) && Input.isKeyTap(Keyboard.Key.V))
@@ -77,65 +97,14 @@ namespace Iris
                     activeField.Append(Clipboard.GetText());
                 }
             }
+
+            //max length for any active field
             if (activeField.Length > 20)
-                activeField.Remove(20, activeField.Length - 20);
-        }
-
-        public override void Draw()
-        {
-            MainGame.window.SetView(MainGame.window.DefaultView);
-            shader.Shader.SetParameter("offsetY", MainGame.Camera.Center.Y);
-            RectangleShape rs = new RectangleShape
             {
-                Size = new Vector2f(800, 600)
-            };
-            MainGame.window.Draw(rs, shader);
-            MainGame.window.SetView(MainGame.Camera);
+                activeField.Remove(20, activeField.Length - 20);
+            }
 
-
-            Render.Draw(Content.GetTexture("background1Far.png"), new Vector2f(-200, -100), Color.White, new Vector2f(0, 0), 1, 0f);
-            Render.Draw(Content.GetTexture("background1Far.png"), new Vector2f(145, -100), Color.White, new Vector2f(0, 0), 1, 0f);
-            Render.Draw(Content.GetTexture("background1.png"), new Vector2f(-200, -150), Color.White, new Vector2f(0, 0), 1, 0f);
-
-            RectangleShape tracks = new RectangleShape(new Vector2f(800, .5f));
-            tracks.Position = new Vector2f(-400, -49);
-            tracks.FillColor = new Color(10, 10, 10, 50);
-            tracks.Draw(MainGame.window, RenderStates.Default);
-
-            Render.Draw(Content.GetTexture("mapDecor.png"), new Vector2f(trainPosX, -55), new Color(255, 255, 255, 200), new Vector2f(0, 0), 1, 0f, .03f);
-
-            Render.Draw(Content.GetTexture("title.png"), new Vector2f(-50, -190), new Color(255, 255, 255, 240), new Vector2f(0, 0), 1, 0f, .4f);
-
-            RectangleShape rectBG = new RectangleShape(new Vector2f(200, 110));
-            rectBG.Position = new Vector2f(-50, 0);
-            rectBG.FillColor = new Color(10, 10, 10, 100);
-            rectBG.Draw(MainGame.window, RenderStates.Default);
-
-            RectangleShape rectUsername = new RectangleShape(new Vector2f(150, 20));
-            rectUsername.Position = new Vector2f(-25, 10);
-            rectUsername.FillColor = new Color(10, 10, 10, (byte)(composingUsername ? 150 : 50));
-            rectUsername.Draw(MainGame.window, RenderStates.Default);
-
-            RectangleShape rectIP = new RectangleShape(new Vector2f(150, 20));
-            rectIP.Position = new Vector2f(-25, 40);
-            rectIP.FillColor = new Color(10, 10, 10, (byte)(composingIP ? 150 : 50));
-            rectIP.Draw(MainGame.window, RenderStates.Default);
-
-            RectangleShape rectConnect = new RectangleShape(new Vector2f(150, 30));
-            rectConnect.Position = new Vector2f(-25, 70);
-            rectConnect.FillColor = new Color(10, 255, 10,
-                (byte)(rectConnect.GetGlobalBounds().Contains(
-                (int)MainGame.worldMousePos.X, (int)MainGame.worldMousePos.Y) ? 150 : 70));
-            rectConnect.Draw(MainGame.window, RenderStates.Default);
-
-            Render.DrawString(Content.GetFont("OldNewspaperTypes.ttf"), usernameField, new Vector2f(50, 15), Color.White, .3f, true, 1);
-
-            Render.DrawString(Content.GetFont("OldNewspaperTypes.ttf"), ipField, new Vector2f(50, 45), Color.White, .3f, true, 1);
-
-            Render.DrawString(Content.GetFont("OldNewspaperTypes.ttf"), "Connect", new Vector2f(50, 77), Color.White, .4f, true, 1);
-
-            Render.Draw(Content.GetTexture("cursorPointer.png"), (Vector2f)MainGame.worldMousePos, Color.White, new Vector2f(0, 0), 1, 0f);
-
+            //tab switching
             if (Input.isKeyTap(Keyboard.Key.Tab))
             {
                 if (composingUsername)
@@ -154,12 +123,13 @@ namespace Iris
                 activeField.Clear();
             }
 
+            //update the textboxes display
             if (composingIP)
                 ipField = activeField.ToString();
             if (composingUsername)
                 usernameField = activeField.ToString();
 
-
+            //reset click
             if (Input.isMouseButtonTap(Mouse.Button.Left))
             {
                 composingUsername = false;
@@ -170,48 +140,107 @@ namespace Iris
                     ipField = "Server IP";
                 activeField.Clear();
 
-            }
+                MainGame.soundInstances.Add(new SoundInstance(Content.GetSound("click.wav"), 1, 0, 5));
 
-            if (rectUsername.GetGlobalBounds().Contains(MainGame.worldMousePos.X, MainGame.worldMousePos.Y))
-            {
-                if (Input.isMouseButtonTap(Mouse.Button.Left))
+                //click to activate username textbox
+                if (rectUsername.GetGlobalBounds().Contains(MainGame.worldMousePos.X, MainGame.worldMousePos.Y))
                 {
-                    MainGame.soundInstances.Add(new SoundInstance(Content.GetSound("click.wav"), 1, 0, 5));
                     composingUsername = true;
                     if (usernameField.Equals("Username"))
                     {
                         usernameField = "";
                     }
                 }
-            }
-            if (rectIP.GetGlobalBounds().Contains(MainGame.worldMousePos.X, MainGame.worldMousePos.Y))
-            {
-                if (Input.isMouseButtonTap(Mouse.Button.Left))
+
+                //else, click to activate ip textbox
+                else if (rectIP.GetGlobalBounds().Contains(MainGame.worldMousePos.X, MainGame.worldMousePos.Y))
                 {
-                    MainGame.soundInstances.Add(new SoundInstance(Content.GetSound("click.wav"), 1, 0, 5));
                     composingIP = true;
                     if (ipField.Equals("Server IP"))
                     {
                         ipField = "";
                     }
                 }
-            }
-            if (rectConnect.GetGlobalBounds().Contains(MainGame.worldMousePos.X, MainGame.worldMousePos.Y))
-            {
-                if (Input.isMouseButtonTap(Mouse.Button.Left))
+
+                //else, click to activate connect button
+                else if (rectConnect.GetGlobalBounds().Contains(MainGame.worldMousePos.X, MainGame.worldMousePos.Y))
                 {
-                    MainGame.soundInstances.Add(new SoundInstance(Content.GetSound("click.wav"), 1, 0, 5));
-                    Submit();
+                    if (BoxesValid)
+                    {
+                        Submit();
+                    }
                 }
             }
+        }
 
+        public override void Draw()
+        {
+            //blue sky
+            MainGame.window.SetView(MainGame.window.DefaultView);
+            shader.Shader.SetParameter("offsetY", MainGame.Camera.Center.Y);
+            RectangleShape rs = new RectangleShape
+            {
+                Size = new Vector2f(800, 600)
+            };
+            MainGame.window.Draw(rs, shader);
+            MainGame.window.SetView(MainGame.Camera);
+
+            //background
+            Render.Draw(Content.GetTexture("background1Far.png"), new Vector2f(-200, -100), Color.White, new Vector2f(0, 0), 1, 0f);
+            Render.Draw(Content.GetTexture("background1Far.png"), new Vector2f(145, -100), Color.White, new Vector2f(0, 0), 1, 0f);
+            Render.Draw(Content.GetTexture("background1.png"), new Vector2f(-200, -150), Color.White, new Vector2f(0, 0), 1, 0f);
+
+            //tracks
+            RectangleShape tracks = new RectangleShape(new Vector2f(800, .5f));
+            tracks.Position = new Vector2f(-400, -49);
+            tracks.FillColor = new Color(10, 10, 10, 50);
+            tracks.Draw(MainGame.window, RenderStates.Default);
+
+            //train
+            Render.Draw(Content.GetTexture("mapDecor.png"), new Vector2f(trainPosX, -55), new Color(255, 255, 255, 200), new Vector2f(0, 0), 1, 0f, .03f);
+
+            //title
+            Render.Draw(Content.GetTexture("title.png"), new Vector2f(-50, -190), new Color(255, 255, 255, 240), new Vector2f(0, 0), 1, 0f, .4f);
+
+            //menubox
+            RectangleShape rectBG = new RectangleShape(new Vector2f(200, 110));
+            rectBG.Position = new Vector2f(-50, 0);
+            rectBG.FillColor = new Color(10, 10, 10, 100);
+            rectBG.Draw(MainGame.window, RenderStates.Default);
+
+            //menu username
+            rectUsername.FillColor = new Color(10, 10, 10, (byte)(composingUsername ? 150 : 50));
+            rectUsername.Draw(MainGame.window, RenderStates.Default);
+
+            //menu ip
+            rectIP.FillColor = new Color(10, 10, 10, (byte)(composingIP ? 150 : 50));
+            rectIP.Draw(MainGame.window, RenderStates.Default);
+
+            //menu connect button
+            rectConnect.FillColor = new Color(10, 255, 10,
+                (byte)(rectConnect.GetGlobalBounds().Contains(
+                (int)MainGame.worldMousePos.X, (int)MainGame.worldMousePos.Y) ? 150 : 70));
+            rectConnect.Draw(MainGame.window, RenderStates.Default);
+
+            //text: username, ip, connect button
+            Render.DrawString(Content.GetFont("OldNewspaperTypes.ttf"), usernameField, new Vector2f(50, 15), Color.White, .3f, true, 1);
+            Render.DrawString(Content.GetFont("OldNewspaperTypes.ttf"), ipField, new Vector2f(50, 45), Color.White, .3f, true, 1);
+            Render.DrawString(Content.GetFont("OldNewspaperTypes.ttf"), "Connect", new Vector2f(50, 77), Color.White, .4f, true, 1);
+
+            //cursor
+            Render.Draw(Content.GetTexture("cursorPointer.png"), MainGame.worldMousePos, Color.White, new Vector2f(0, 0), 1, 0f);
+        }
+
+        public bool BoxesValid
+        {
+            get
+            {
+                return ipField.Trim() != "Server IP" && usernameField.Trim() != "Username";
+            }
         }
 
         public void Submit()
         {
-            if (ipField.Trim().Equals("Server IP"))
-                return;
-
             ClientMailman.ip = ipField;
 
             if (MainGame.dm.Mailman.Connect())
@@ -219,15 +248,12 @@ namespace Iris
                 MainGame.gamestate = MainGame.dm;
                 MainGame.window.TextEntered -= TextEnteredEvent;
                 MainGame.dm.player.Name = usernameField;
-
             }
             else
             {
                 MainGame.soundInstances.Add(new SoundInstance(Content.GetSound("error.wav"), 1, 0, 10));
                 ipField = "Failed to Connect";
             }
-
         }
-
     }
 }
