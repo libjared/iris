acee9ee53eb1aa5ffc039b953eb7e4dcf25bb406
sourcerect <hypershadsy@gmail.com>
2015-10-12 14:20:57 -0400

    ShieldGenerator radius is func(time)

--- Iris/Classes/ShieldGenerator.cs
+++ Iris/Classes/ShieldGenerator.cs
@@ -12,40 +12,58 @@ namespace Iris
     public class ShieldGenerator : GameObject
     {
 
-        Texture tex, col, shield;
-        float size, radius;
+        Texture tex, col;
         private RenderStates shader;
+        readonly DateTime whenPlaced;
+        readonly TimeSpan lifespan = new TimeSpan(0,0,10); //lasts for 10 sec
+        const float radiusStart = 125f;
+        const float radiusEnd = 40f;
+        float Radius
+        {
+            get
+            {
+                var now = DateTime.Now;
+                var then = whenPlaced + lifespan;
+                var timeSince = now - whenPlaced;
+                var weight = timeSince.Ticks / (float)lifespan.Ticks;
+                float rad = Helper.Lerp(radiusStart, radiusEnd, weight);
+                return rad;
+            }
+        }
+        bool TimesUp
+        {
+            get
+            {
+                return DateTime.Now.CompareTo(whenPlaced + lifespan) != -1;
+            }
+        }
+
 
         public ShieldGenerator(Vector2f position)
         {
             tex = Content.GetTexture("generatorStand.png");
             col = Content.GetTexture("genBlue.png");
-            shield = Content.GetTexture("shield.png");
-            var sh = new Shader(null, "Content/shield.frag");
-            shader = new RenderStates(sh);
-            this.Pos = position;
-            size = .35f;
+            shader = new RenderStates(new Shader(null, "Content/shield.frag"));
+            whenPlaced = DateTime.Now;
+            Pos = position;
         }
 
         public override void Update()
         {
-            radius = size * .45f * shield.Size.X;
             for (int i = 0; i < MainGame.dm.Projectiles.Count; i++)
             {
                 Projectile p = MainGame.dm.Projectiles[i];
-                if (Helper.Distance(p.Pos, this.Pos) < (radius))
+                if (Helper.Distance(p.Pos, this.Pos) < Radius)
                 {
                     MainGame.dm.Projectiles[i].Destroy();
                 }
             }
-            size -= .00025f;
 
-            if (size <= 0)
+            if (TimesUp)
             {
                 MainGame.dm.GameObjects.Add(new Explosion(this.Pos - new Vector2f(20, 0), MainGame.dm.player, true));
                 MainGame.dm.GameObjects.Remove(this);
             }
-
 
             base.Update();
         }
@@ -61,7 +79,7 @@ namespace Iris
             float seconds = (float)(DateTime.Now - MainGame.startTime).TotalMilliseconds / 1000f;
             shader.Shader.SetParameter("center", centerf);
             shader.Shader.SetParameter("seconds", seconds);
-            Render.DrawCircle(Pos, radius);
+            Render.DrawCircle(Pos, Radius);
             Render.renderStates = null;
             base.Draw();
         }
